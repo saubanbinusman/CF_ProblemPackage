b21c9c2805326ff82d37d5e728b746a3e8edb559
Sauban bin Usman <saubanbinusman@yahoo.com>
2019-11-12 22:12:13 +0500

    Auto fetches JSESSIONID, csrfToken. +Major Refactor

--- Builder.py
+++ Builder.py
@@ -1,97 +1,139 @@
-#!/bin/python
-
 import os
+import re
 import csv
 import sys
 import json
+import time
 import random
 import shutil
 import zipfile
 import requests
 
-def retrieve_file_paths(dirName):
-	filePaths = []
-	
-	for root, directories, files in os.walk(dirName):
-		for filename in files:
-			filePath = os.path.join(root, filename)
-			filePaths.append(filePath)
-	
-	return filePaths
+class CFService:
+	login_api = "https://codeforces.com/enter"
+	submission_api = "https://codeforces.com/data/submitSource"
 
-with open("input.csv", "r") as input_csv:
-	csv_reader = csv.reader(input_csv, delimiter=',')
-	line_count = 0
+	def __init__(self):
+		self.session = requests.Session()
+
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
+		}
+
+		# Open codeforces main page to get a JSESSIONID cookie and csrf_token from the html
+		response = self.session.get("https://codeforces.com/", headers=self.headers).text
+		self.csrf_token = re.search("<meta name=\"X-Csrf-Token\" content=\"([^\"]+)\"\/>", response).groups()[0]
+
+
+	def get_submission(self, submission_id):
+		time.sleep(0.5)  # To avoid flooding the server with requests (in case of long CSV files)
+
+		response = self.session.post(self.submission_api, data={ "submissionId": submission_id, "csrf_token": self.csrf_token }, headers=self.headers).json()
+
+		ac_source = response["source"]  # The source code of the submission
+
+		test_count = int(response["testCount"])  # Total number of test cases
+
+		test_inputs  = [s.replace("\r", "") for s in [response["input#"  + str(i)] for i in range(1, test_count + 1)]]
+		test_answers = [s.replace("\r", "") for s in [response["answer#" + str(i)] for i in range(1, test_count + 1)]]
+
+		return ac_source, test_inputs, test_answers
+
+
+def retrieve_file_paths(dir_name):
+	file_paths = []
 	
-	for row in csv_reader:
-		if (line_count == 0):
-			line_count += 1
-		else:
-			print(row[0] + " " + row[1] + " " + row[2], end=" --- ")
+	for root, directories, files in os.walk(dir_name):
+		for filename in files:
+			file_path = os.path.join(root, filename)
+			file_paths.append(file_path)
+	
+	return file_paths
+
+def main():
+	cf_service = CFService()
+
+	with open("input.csv", "r") as input_csv:
+		csv_reader = csv.reader(input_csv)
+
+		next(csv_reader)  # Ignore CSV headers line
+		
+		for row in csv_reader:
+			problem_id     = row[0] + "_" + row[1]
+			submission_url = row[2]
+			submission_id  = submission_url[submission_url.rindex("/") + 1:]
+			problem_name   = row[3]
+			time_limit     = row[4]
+
+			print(F"- {problem_id} {submission_url}")
 			
-			dir_main = "temp"
-			os.makedirs(dir_main, exist_ok=True)
+			# Create a temporary directory to store Problem Package Structure
+			dir_top_level = "temp"
+			os.makedirs(dir_top_level, exist_ok=True)
 			
-			problem_id = row[0] + "_" + row[1]
-			problem_name = row[3]
-			time = row[4]
-			
-			problem_desc = "probid = \"{}\"\nname = \"{}\"\nallow_submit = 1\nallow_judge = 1\ntimelimit = {}\ncolor = {}"
-			
+			# Generate a random color '#xxxxxx' for Problem
 			r = lambda: random.randint(0,255)
 			color = "#{:02x}{:02x}{:02x}".format(random.randint(0,255), random.randint(0,255), random.randint(0,255))
 			
-			problem_desc = problem_desc.format(problem_id, problem_name, time, color)
-			
-			with open(dir_main + "/domjudge-problem.ini", "w") as descFile:
-				descFile.write(problem_desc)
-			
-			dir_tests = dir_main + "/data/secret"
-			os.makedirs(dir_tests, exist_ok=True)
-			
-			dir_sol = dir_main + "/submissions/accepted"
-			os.makedirs(dir_sol, exist_ok=True)
-
-			submission_id = row[2][row[2].rindex("/") + 1:]
-
-			session_id = "314BCC085FA3A5660ED6C598FA518048-n1"  # Change with your JSESSIONID cookie
-			csrf_token = "b419345ea7f5e2f3d7037660abe69bfb"  # Change with your csrf token
-
-			submission_api = "https://codeforces.com/data/submitSource"
-			web_header = {
-				"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0",
-				"accept": "*/*",
-				"Cookie":  "JSESSIONID=" + session_id,
-				"X-Csrf-Token": csrf_token
+			# Write Problem Description file
+			problem_desc = {
+				"probid": '"' + problem_id + '"',
+				"name"  : '"' + problem_name + '"',
+				"allow_judge": 1,
+				"allow_submit": 1,
+				"timelimit": time_limit,
+				"color": color
 			}
 
-			submission_data = requests.post(submission_api, data={ "submissionId": submission_id }, headers=web_header).json()
+			with open(F"{dir_top_level}/domjudge-problem.ini", "w") as desc_file:
+				for k, v in problem_desc.items():
+					desc_file.write(F"{k} = {v}\n")
+			
+			print( "\t-- Written domjudge-problem.ini")
+			
+			# Create Directory for Test Cases
+			dir_tests = F"{dir_top_level}/data/secret"
+			os.makedirs(dir_tests, exist_ok=True)
+			
+			# Create Directory for AC Solution (not required, but helpful in testing)
+			dir_sol = F"{dir_top_level}/submissions/accepted"
+			os.makedirs(dir_sol, exist_ok=True)
 
+			# Fetch submission data from Codeforces
+			ac_source, inputs, answers = cf_service.get_submission(submission_id)
+			print( "\t-- Fetched Submission Details")
+
+			# Write AC Solution to file
 			with open(dir_sol + "/sol.cpp", "w") as sol_file:
-				sol_file.write(submission_data["source"])
+				sol_file.write(ac_source)
+			
+			print( "\t-- Written sol.cpp")
+			
+			# Write Test Cases to files
+			for tc, inp, ans in zip(range(1, len(inputs) + 1), inputs, answers):
+				with open(F"{dir_tests}/{tc}.in", "w") as inp_file, open(F"{dir_tests}/{tc}.ans", "w") as ans_file:
+					inp_file.write(inp)
+					ans_file.write(ans)
+			
+			print(F"\t-- Written {len(inputs)} Test Cases")
 
-			test_count = int(submission_data["testCount"])
-
-			ins  = [s.replace("\r", "") for s in [submission_data["input#"  + str(i)] for i in range(1, test_count + 1)]]
-			outs = [s.replace("\r", "") for s in [submission_data["answer#" + str(i)] for i in range(1, test_count + 1)]]
-
-			i = 1
+			# Enlist file paths to be zipped
+			files_to_zip = retrieve_file_paths(dir_top_level)
 			
-			for a, b in zip(ins, outs):
-				with open(dir_tests + "/" + repr(i) + ".in", "w") as inFile:
-					inFile.write(a)
-				
-				with open(dir_tests + "/" + repr(i) + ".ans", "w") as outFile:
-					outFile.write(b)
-				
-				i = i + 1
-			
-			print("Fetched " + str(test_count) + " TCs. Zipping ...")
-			
-			files_to_zip = retrieve_file_paths(dir_main)
-			
+			# Zip files into a problem package
 			with zipfile.ZipFile(problem_id + ".zip", "w") as zip_file:
 				for file in files_to_zip:
-					zip_file.write(file, file.replace(dir_main, ""))
+					zip_file.write(file, file.replace(dir_top_level, ""))
 			
-			shutil.rmtree(dir_main)
+			print(F"\t-- Zipped {problem_id}.zip")
+			
+			# Remove temporary directory
+			shutil.rmtree(dir_top_level)
+
+			print( "\t-- Cleaned Temporary Files")
+
+			print( "\n\tProblem Package Created\n")
+
+
+if __name__ == "__main__":
+	main()
